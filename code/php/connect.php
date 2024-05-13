a4dead821dd0a11d807b60cb7eb9bc468876b2de
yukiiso <yukiiso@student.ubc.ca>
2024-05-13 15:22:12 +0900

    Modified DB configs

--- code/php/connect.php
+++ code/php/connect.php
@@ -1,13 +1,13 @@
 <?php
-    // $host = "localhost";
-    // $database = "project";
-    // $user = "root";
-    // $password = "";
+    $host = "localhost";
+    $database = "project";
+    $user = "root";
+    $password = "";
 
-    $host = "cosc360.ok.ubc.ca";
-    $database = "db_11888757";
-    $user = "11888757";
-    $password = "11888757";
+    // $host = "cosc360.ok.ubc.ca";
+    // $database = "db_11888757";
+    // $user = "11888757";
+    // $password = "11888757";
     
     $connection = mysqli_connect($host, $user, $password, $database);
     
--- code/php/db_config.php
+++ code/php/db_config.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "project";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project";
 
-$servername = "cosc360.ok.ubc.ca";
-$username = "11888757";
-$password = "11888757";
-$dbname = "db_11888757";
+// $servername = "cosc360.ok.ubc.ca";
+// $username = "11888757";
+// $password = "11888757";
+// $dbname = "db_11888757";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
